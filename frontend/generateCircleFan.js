a87f10a96c350289658a47977cdadffa7f760840
Kseroten1 <klein.damian46@gmail.com>
2025-09-03 14:53:54 +0200

    review fix

--- frontend/generateCircleFan.js
+++ frontend/generateCircleFan.js
@@ -1,5 +1,6 @@
+import { ELEMENTS_PER_VERTEX } from './script.js';
+const twoPI = Math.PI * 2;
 export function generateCircleFan({ cx, cy, radius, segments }) {
-    import ELEMENTS_PER_VERTEX from './script.js';
     const count = 2 + segments; // center and closing + ring 
     const data = new Float32Array(count * ELEMENTS_PER_VERTEX); // count is the number of points, we need to multiply in this case for x and y 
     
@@ -7,8 +8,7 @@ export function generateCircleFan({ cx, cy, radius, segments }) {
     
     data[o++] = cx;
     data[o++] = cy;
-    
-    const twoPI = Math.PI * 2;
+
     for (let i = 0; i <= segments; i++) {
         const t = (i/segments) * twoPI;
         const x = cx + radius * Math.cos(t);
