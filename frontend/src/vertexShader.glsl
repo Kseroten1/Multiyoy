be4613c71f857fd1f38015cd32d1fc9802ee7b04
Mateusz Budzisz <electroluxv2@gmail.com>
2025-09-14 15:35:29 +0200

    more readable

--- frontend/src/vertexShader.glsl
+++ frontend/src/vertexShader.glsl
@@ -1,20 +1,19 @@
 #version 300 es
-precision highp float; 
+precision highp float;
 
 const vec2 HEX_OFFSETS[8] = vec2[](
     // center
     vec2(0.0, 0.0),
 
-    // 6 corners, pointy-top (start at 30°, step 60°)
-    vec2(cos(30.0), sin(30.0)),   // 30°  = (cos 30,  sin 30)
-    vec2(cos(90.0), sin(90.0)),   // 90°
-    vec2(cos(150.0), sin(150.0)),   // 150°
-    vec2(cos(210.0), sin(210.0)),   // 210°
-    vec2(cos(270.0), sin(270.0)),   // 270°
-    vec2(cos(330.0), sin(330.0)),   // 330°
+    vec2(cos(radians(30.0)), sin(radians(30.0))),  
+    vec2(cos(radians(90.0)), sin(radians(90.0))),  
+    vec2(cos(radians(150.0)), sin(radians(150.0))),
+    vec2(cos(radians(210.0)), sin(radians(210.0))),
+    vec2(cos(radians(270.0)), sin(radians(270.0))),
+    vec2(cos(radians(330.0)), sin(radians(330.0))),
 
     // repeat first corner to close the fan
-    vec2( 0.8660254,  0.5)
+    vec2(cos(radians(30.0)), sin(radians(30.0)))
 );
 
 uniform mat3 u_mvp;
