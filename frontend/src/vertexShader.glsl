a7c5efa8f7f9797d4d2c80de473499160d5a3d60
Kseroten1 <klein.damian46@gmail.com>
2025-09-16 20:51:23 +0200

    drawing 7 hexes, with correct corners order

--- frontend/src/vertexShader.glsl
+++ frontend/src/vertexShader.glsl
@@ -1,20 +1,16 @@
 #version 300 es
-precision highp float; 
+precision highp float;
 
 const vec2 HEX_OFFSETS[8] = vec2[](
-    // center
-    vec2(0.0, 0.0),
+vec2(0.0, 0.0),
+vec2( 0.0,        1.0),   // 90°
+vec2( 0.8660254,  0.5),   // 30°
+vec2( 0.8660254, -0.5),   // 330°
+vec2( 0.0,       -1.0),   // 270°
+vec2(-0.8660254, -0.5),   // 210°
+vec2(-0.8660254,  0.5),   // 150°
 
-    // 6 corners, pointy-top (start at 30°, step 60°)
-    vec2( 0.8660254,  0.5),   // 30°  = (cos 30,  sin 30)
-    vec2( 0.0,        1.0),   // 90°
-    vec2(-0.8660254,  0.5),   // 150°
-    vec2(-0.8660254, -0.5),   // 210°
-    vec2( 0.0,       -1.0),   // 270°
-    vec2( 0.8660254, -0.5),   // 330°
-
-    // repeat first corner to close the fan
-    vec2( 0.8660254,  0.5)
+vec2( 0.0,        1.0)   // 90°
 );
 
 uniform mat3 u_mvp;
