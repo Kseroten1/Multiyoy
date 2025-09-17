39befffd80946cd7006112ff3ec4a6f2566fab73
Kseroten1 <klein.damian46@gmail.com>
2025-09-17 23:50:31 +0200

    fixed vertices order

--- frontend/src/vertexShader.glsl
+++ frontend/src/vertexShader.glsl
@@ -3,15 +3,15 @@ precision highp float;
 
 const vec2 HEX_OFFSETS[8] = vec2[](
   vec2(0.0, 0.0),                                   // V0 (center)
-    // 6 corners, pointy-top (start at 30°, step 60°)
-vec2(cos(radians(90.0)),   sin(radians(90.0))),    // 90°
-vec2(cos(radians(30.0)),   sin(radians(30.0))),    // 30°
-vec2(cos(radians(330.0)),  sin(radians(330.0))),   // 330°
+    	// 6 corners, pointy-top (start at 30°, step 60°)
 vec2(cos(radians(270.0)),  sin(radians(270.0))),   // 270°
 vec2(cos(radians(210.0)),  sin(radians(210.0))),   // 210°
 vec2(cos(radians(150.0)),  sin(radians(150.0))),   // 150°
+vec2(cos(radians(90.0)),   sin(radians(90.0))),    // 90°
+vec2(cos(radians(30.0)),   sin(radians(30.0))),    // 30°
+vec2(cos(radians(330.0)),  sin(radians(330.0))),   // 330°
 
-vec2(cos(radians(90.0)),   sin(radians(90.0)))     // 90°
+vec2(cos(radians(270.0)),  sin(radians(270.0)))   // 270°
 );
 
 uniform mat3 u_mvp;
