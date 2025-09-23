caa2611a1a0e62245c0b32a072c65e1032a55e82
Kseroten1 <klein.damian46@gmail.com>
2025-09-23 20:35:49 +0200

    post review fix

--- frontend/src/fragmentShader.glsl
+++ frontend/src/fragmentShader.glsl
@@ -8,14 +8,23 @@ uniform uint  u_edgeMask; // maska krawedzi
 in vec2 v_local;
 flat in int v_edgeId;
 out vec4 outColor;
-int bit(uint mask, int i) { return int((mask >> uint(i)) & uint(1)); }
+// Zwraca i-ty bit maski (0 lub 1). Zabezpiecza indeks.
+int getBitAt(uint mask, int index) {
+    uint shifted = mask >> uint(index); //przesuń bit o indeksie podanym na pozycję LSB (least significant bit)
+    uint lsb     = shifted & uint(1); // operacja AND czyli wyzerowanie wszystkich bitów poza pozycją 0 czyli LSB 
+    return int(lsb);
+}
 
 //source : https://iquilezles.org/articles/distfunctions2d/
 float sdHex(vec2 position) {
-    const float scale = 1.0 / 0.8660254037844386;
+    const float scale = 2.0 / sqrt(3.0);
     position *= scale;
     position = vec2(-position.y, position.x);
-    const vec3 basis = vec3(-0.8660254, 0.5, 0.577350269);
+    const vec3 basis = vec3(
+    -sqrt(3.0) / 2.0,
+    1.0 / 2.0,
+    1.0 / sqrt(3.0)
+    );
     position = abs(position);
     float projection = dot(basis.xy, position);
     position -= 2.0 * min(projection, 0.0) * basis.xy;
@@ -40,9 +49,19 @@ void main() {
     float distanceSq = sdHex(v_local);
     const float borderWidth = 0.06;
     
-    float isCurrentPixelOnBorder = step(distanceSq, borderWidth * borderWidth);
-    vec3 edgeColor = mix(fillColor, EDGE_COLORS[v_edgeId], float(bit(u_edgeMask, v_edgeId)));
-	vec3 pixelColor = mix(fillColor, edgeColor, isCurrentPixelOnBorder);
+    float borderRadiusSquared = borderWidth * borderWidth;
+    // 0.0 lub 1.0: czy piksel leży w obszarze obrysu (<= grubości)
+    float isCurrentPixelOnBorder = step(distanceSq, borderRadiusSquared);
+    vec3 edgeColor = mix(
+        fillColor, // kolor bazowego wypełnienia
+        EDGE_COLORS[v_edgeId], // kolor krawędzi dla danego wierzchołka
+        float(getBitAt(u_edgeMask, v_edgeId)) // czy maska włączona 
+    );
+	vec3 pixelColor = mix(
+        fillColor, // kolor gdy poza obrysem 
+        edgeColor, // kolor gdy na obrysie 
+        isCurrentPixelOnBorder // czy na obrysie
+    );
 
     outColor = vec4(pixelColor, 1.0);
 }
