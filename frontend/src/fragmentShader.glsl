4ff3a61e81c9b1e27f4d03d21d909c1468bb1b5b
Kseroten1 <klein.damian46@gmail.com>
2025-10-25 12:00:23 +0200

    working branchless, maskless

--- frontend/src/fragmentShader.glsl
+++ frontend/src/fragmentShader.glsl
@@ -6,11 +6,13 @@ uniform vec3  u_colorB;  // dolny color
 uniform uint  u_edgeMask;  // maska krawedzi 
 uniform float u_borderWidth;  // szerokość krawedzi w jednostkach lokalnych
 
+flat in int vertexID;
 in vec2 v_local;
 out vec4 outColor;
 
 const float cos60 = cos(radians(60.0));
 const float sin60 = sin(radians(60.0));
+const vec3 fillColor = vec3(1.0, 1.0, 1.0);
 
 // Zwraca i-ty bit maski (0 lub 1).
 int getBitAt(uint mask, int index) {
@@ -19,50 +21,48 @@ int getBitAt(uint mask, int index) {
     return int(hexSideMaskOn);
 }
 
-const vec2 HEX_VERTICES[6] = vec2[](
-vec2(cos(radians(270.0)),  sin(radians(270.0))),
-vec2(cos(radians(330.0)),  sin(radians(330.0))),
-vec2(cos(radians(30.0)),   sin(radians(30.0))),
-vec2(cos(radians(90.0)),   sin(radians(90.0))),
-vec2(cos(radians(150.0)),  sin(radians(150.0))),
-vec2(cos(radians(210.0)),  sin(radians(210.0)))
+const vec2 HEX_OFFSETS[8] = vec2[](
+    vec2(0.0, 0.0),   // V0 (center)
+    vec2(cos(radians(90.0)),   sin(radians(90.0))),   // V1 – góra
+    vec2(cos(radians(30.0)),   sin(radians(30.0))),   // V2 – prawy‑góra
+    vec2(cos(radians(330.0)),  sin(radians(330.0))),  // V3 – prawy‑dół
+    vec2(cos(radians(270.0)),  sin(radians(270.0))),  // V4 – dół
+    vec2(cos(radians(210.0)),  sin(radians(210.0))),  // V5 – lewy‑dół
+    vec2(cos(radians(150.0)),  sin(radians(150.0))),  // V6 – lewy‑góra
+    vec2(cos(radians(90.0)),   sin(radians(90.0)))    // powtórka V1 – domknięcie
 );
 
 const vec3 EDGE_COLORS[6] = vec3[](
-  vec3(1.0, 0.0, 0.0),  // 1 czerwony
-  vec3(1.0, 0.5, 0.0),  // 2 pomarańczowy
-  vec3(1.0, 1.0, 0.0),  // 3 żółty
-  vec3(0.0, 1.0, 0.0),  // 4 zielony
-  vec3(0.0, 0.5, 1.0),  // 5 niebieski
-  vec3(0.6, 0.0, 1.0)   // 6 fioletowy
+    vec3(1.0, 0.0, 0.0),  // 0 czerwony
+    vec3(1.0, 0.5, 0.0),  // 1 pomarańczowy
+    vec3(1.0, 1.0, 0.0),  // 2 żółty
+    vec3(0.0, 1.0, 0.0),  // 3 zielony
+    vec3(0.0, 0.5, 1.0),  // 4 niebieski
+    vec3(0.6, 0.0, 1.0)   // 5 fioletowy
 );
 
-float pointDistanceFromLine(vec2 point, vec2 firstVertex, vec2 secondVertex) {
+float pointRelativeDistanceFromLine(vec2 point, vec2 firstVertex, vec2 secondVertex) {
     // Współczynniki prostej opisanej na dwóch punktach (firstVertex, secondVertex)
     // Ax + By + C - wzór prostej 
     float A = firstVertex.y - secondVertex.y;
-    float B = firstVertex.x - secondVertex.x;
+    float B = secondVertex.x - firstVertex.x;
     float C = firstVertex.x * secondVertex.y - secondVertex.x * firstVertex.y;
-    
     // zwracamy odległość punktu od prostej wzór:
     // punkt (x0, y0)
     // (|Ax0 + By0 + C|)/sqrt(A^2+B^2)
-    return abs(A * point.x + B * point.y + C) / sqrt(A * A + B * B);
+    // !!! fajne zadanie na kolokwium
+    // abs(A * point.x + B * point.y + C) / sqrt(A*A + B*B);
+    return -(A * point.x + B * point.y + C);
 }
 
 void main() {
-    float isTop = step(v_local.y, 0.0);
-    vec3 fillColor = mix(u_colorA, u_colorB, isTop);
-    vec3 outRgb = fillColor;
-    for (int i = 0; i < 6; ++i) {
-        if (getBitAt(u_edgeMask, i) == 1) {
-            vec2 vertex0 = HEX_VERTICES[i];
-            vec2 vertex1 = HEX_VERTICES[(i + 1) % 6];
-            float dist = pointDistanceFromLine(v_local, vertex0, vertex1);
-            if (dist < u_borderWidth) {
-                outRgb = EDGE_COLORS[i];
-            }
-        }
-    }
-    outColor = vec4(outRgb, 1.0);
+    vec2 ver1 = HEX_OFFSETS[vertexID - 1];
+    vec2 ver2 = HEX_OFFSETS[vertexID];
+    float dist = pointRelativeDistanceFromLine(v_local, ver1, ver2);
+    vec3 color = mix(
+        fillColor,
+        EDGE_COLORS[vertexID - 2],
+        step(dist, u_borderWidth)
+    );
+    outColor = vec4(color, 1.0);
 }
--- frontend/src/vertexShader.glsl
+++ frontend/src/vertexShader.glsl
@@ -2,23 +2,24 @@
 precision highp float; 
 
 const vec2 HEX_OFFSETS[8] = vec2[](
-vec2(0.0, 0.0),   // V0 (center)
-// 6 corners, pointy-top (start at 30°, step 60°)
-vec2(cos(radians(270.0)),  sin(radians(270.0))),   // 270°
-vec2(cos(radians(210.0)),  sin(radians(210.0))),   // 210°
-vec2(cos(radians(150.0)),  sin(radians(150.0))),   // 150°
-vec2(cos(radians(90.0)),   sin(radians(90.0))),    // 90°
-vec2(cos(radians(30.0)),   sin(radians(30.0))),    // 30°
-vec2(cos(radians(330.0)),  sin(radians(330.0))),   // 330°
-vec2(cos(radians(270.0)),  sin(radians(270.0)))    // 270°
+    vec2(0.0, 0.0),   // V0 (center)
+    vec2(cos(radians(90.0)),   sin(radians(90.0))),   // V1 – góra
+    vec2(cos(radians(30.0)),   sin(radians(30.0))),   // V2 – prawy‑góra
+    vec2(cos(radians(330.0)),  sin(radians(330.0))),  // V3 – prawy‑dół
+    vec2(cos(radians(270.0)),  sin(radians(270.0))),  // V4 – dół
+    vec2(cos(radians(210.0)),  sin(radians(210.0))),  // V5 – lewy‑dół
+    vec2(cos(radians(150.0)),  sin(radians(150.0))),  // V6 – lewy‑góra
+    vec2(cos(radians(90.0)),   sin(radians(90.0)))    // powtórka V1 – domknięcie
 );
 
 uniform mat3 u_mvp;
 uniform vec2 u_center;
 out vec2 v_local;
+flat out int vertexID;
 
 void main() {
     vec2 localPos = HEX_OFFSETS[gl_VertexID];
+    vertexID = gl_VertexID;
     v_local = localPos;
     vec2 modelPos = u_center + localPos;
     vec3 clipPos = u_mvp * vec3(modelPos, 1.0);
